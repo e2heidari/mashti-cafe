eee2d7352dd1bb47ef1e423a5ddf85ecf80205d3
Ehsan heidari <ehsan.heydari@gmail.com>
2025-08-19 20:47:37 -0700

    Add redirects for old WordPress URLs

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,50 @@ const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ['next-sanity'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/all-about-creativity-and-inspiration/',
+        destination: '/blog/all-about-creativity-and-inspiration',
+        permanent: true,
+      },
+      {
+        source: '/7-facts-every-business-should-know/',
+        destination: '/blog/7-facts-every-business-should-know',
+        permanent: true,
+      },
+      {
+        source: '/top-20-small-business-blogs-to-follow/',
+        destination: '/blog/top-20-small-business-blogs-to-follow',
+        permanent: true,
+      },
+      {
+        source: '/tips-to-move-your-project-more-forward/',
+        destination: '/blog/tips-to-move-your-project-more-forward',
+        permanent: true,
+      },
+      {
+        source: '/online-reputation-and-management/',
+        destination: '/blog/online-reputation-and-management',
+        permanent: true,
+      },
+      {
+        source: '/how-to-be-ahead-of-stock-changes/',
+        destination: '/blog/how-to-be-ahead-of-stock-changes',
+        permanent: true,
+      },
+      {
+        source: '/category/:slug/',
+        destination: '/blog/category/:slug',
+        permanent: true,
+      },
+      {
+        source: '/tag/:slug/',
+        destination: '/blog/tag/:slug',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
